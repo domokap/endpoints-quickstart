fadc1993c4568212b5db2d7bd14c62410b5f3c39
domokap <dominykas.kapleraitis@sky.uk>
2023-06-23 11:40:15 +0100

    added response count and ack

--- app/finbot.py
+++ app/finbot.py
@@ -9,7 +9,7 @@ def process_response(body):
     for action in body["actions"]:
         if "response_message" in action["action_id"]:
             if process_message(action, body["message"]["metadata"], body["user"]["username"]):
-                respond(response)
+                respond(response, action)
         elif "response_button" in action["action_id"]:
             process_button()
         else:
@@ -28,7 +28,7 @@ def process_message(action, metadata, user):
         return True
     elif metadata["event_type"] == "anomaly_report":
         acc_id = action["action_id"]
-        account = acc_id[acc_id.index("-")+1:]
+        account = acc_id[acc_id.index("/")+1:]
         response["ids"] = response["ids"][account]
         write_to_gcs(response, "anomaly_responses.jsonl", "anomaly")
         return True
@@ -58,19 +58,23 @@ def write_to_gcs(payload, file, report_type):
     blob.upload_from_string(contents)
     return True
 
-def respond(response):
+def respond(response, action):
     # payload = """{
     #     "replace_original": false,
     #     "text": "RESPONSE RECEIVED",
     #     "response_type": "in_channel"
     # }"""
     # print(requests.post(response["response_url"], json=json.loads(payload)).json())
-    payload = json.dumps({
+    payload = {
         "replace_original": True,
-        "text": response["message"]["text"] + " TEST",
+        "text": response["message"]["text"],
         "blocks": response["message"]["blocks"],
         "metadata": response["message"]["metadata"]
-    })
+    }
+    if payload["metadata"]["event_payload"][action["action_id"]] == 0:
+        next(i for i in response["message"]["blocks"] if i["block_id"] == action["block_id"])["label"]["text"] += " :white_tick:"
+    payload["metadata"]["event_payload"][action["action_id"]] = payload["metadata"]["event_payload"].get(action["action_id"], 0) + 1
+    payload = json.dumps(payload)
     print(payload)
     print(requests.post(response["response_url"], json=json.loads(payload)).json())
     return True
