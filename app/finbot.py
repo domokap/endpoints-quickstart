e9e2cb283d2c6196be98c206cf3c19d028e0ed55
domokap <dominykas.kapleraitis@sky.uk>
2023-06-26 17:09:54 +0100

    elective filtering

--- app/finbot.py
+++ app/finbot.py
@@ -23,7 +23,8 @@ def process_message(action, metadata, user):
     if action["value"] is None:
         return False
     # response = metadata["event_payload"]
-    response = {k:v for k,v in metadata["event_payload"] if "response_message" not in k}
+    # response = {k:v for k,v in metadata["event_payload"] if "response_message" not in k}
+    response = {k:v for k,v in metadata["event_payload"].items() if k in ["team", "ids"]}
     response["message"] = action["value"]
     response["user"] = user
     response["responseTime"] = datetime.fromtimestamp(int(float(action["action_ts"]))).isoformat()
