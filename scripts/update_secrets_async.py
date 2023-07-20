20c4bed1c1ece8dcb10f57f33074ee528507165f
domokap <dominykas.kapleraitis@sky.uk>
2023-07-20 15:14:48 +0100

    error out added

--- scripts/update_secrets_async.py
+++ scripts/update_secrets_async.py
@@ -17,19 +17,21 @@ default_bucket = "nbcu-finops-data-repo"
 
 
 def update_secrets(bucket=default_bucket, function_blob="finbot_functions.json", team_blob="finbot_config.json"):
-  print("Getting function config")
+  print(f"Getting {function_blob}")
   functions = get_blob(bucket, function_blob)
-  print("Getting team config")
+  if functions is False: return False
+  print(f"Getting {team_blob}")
   teams = get_blob(bucket, team_blob)
-  print("Getting function descriptions")
+  if teams is False: return False
+  print("Gathering function descriptions")
   descriptions = describe(functions)
+  print("Compiling secrets")
   current_secrets  = {function:{secret["key"]:secret["secret"] for secret in description.get("secretEnvironmentVariables",{})} for function,description in descriptions.items()}
   expected_secrets = {function:get_expected_secrets(function, func_conf, teams) for function, func_conf in functions.items()}
   missing_secrets = {function:{k:v for k,v in expected_secrets[function].items() if (k,v) not in current_secrets[function].items()} for function in functions.keys()}
   commands = [f"gcloud functions deploy {function} --update-secrets='{secret_arg(missing_secrets[function])}' --trigger-topic='{func_conf['trigger-topic']}' --runtime=python310 --source='gs://{get_source(function,func_conf['region'])}' --entry-point='{func_conf['entry-point']}' --region='{func_conf['region']}'" for function, func_conf in functions.items() if missing_secrets[function] != {}]
   todo = len(commands); ticker = 0
-  print("Updating ", end="")
-  print(*(i for i in functions if missing_secrets[i] != {}), sep=", ")
+  print("Updating ", end=""); print(*(i for i in functions if missing_secrets[i] != {}), sep=", ")
   running_cmds = [Popen(i, stdout=PIPE, stderr=PIPE, shell=True) for i in commands]
   while running_cmds:
     for cmd in running_cmds:
